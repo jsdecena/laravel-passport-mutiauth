f02563e70b74d4a86ed273275e2d6e863e932012
Jeff Simons Decena <jeff.decena@yahoo.com>
2017-10-13 08:33:01 +0800

    Validate if request has the username / provider

--- src/Middleware/ProviderDetectorMiddleware.php
+++ src/Middleware/ProviderDetectorMiddleware.php
@@ -13,9 +13,19 @@ class ProviderDetectorMiddleware
      */
     public function handle(Request $request, \Closure $next)
     {
-        if (array_key_exists('provider', $request->all())) {
-            config(['auth.guards.api.provider' => $request->input('provider')]);
+        $validator = validator()->make($request->all(), [
+            'username' => 'required',
+            'provider' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->getMessageBag(),
+                'status_code' => 422
+            ], 422);
         }
+
+        config(['auth.guards.api.provider' => $request->input('provider')]);
 
         return $next($request);
     }
